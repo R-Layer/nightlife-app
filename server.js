93538eb35725806083f7533a7d65eb0e2a5f21f7
R-Layer <layer@europe.com>
2018-07-09 20:56:13 +0200

    Deploy setup

---
+++ server.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const configVars = require("./config/keys");
+const userRoutes = require("./routes/userRoutes");
+
+const port = configVars.PORT;
+
+mongoose
+  .connect(
+    configVars.MONGODB_URI,
+    (() => console.log("Mongodb connected!"), { useNewUrlParser: true })
+  )
+  .catch(err => {
+    console.log("Connection error: ", err);
+    process.exit(1);
+  });
+
+const app = express();
+
+app.use(bodyParser.json());
+
+app.use("/users", userRoutes);
+
+app.get("/", (req, res) => {
+  res.send({ message: "kut!" });
+});
+
+// Server static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Server running at ${port}`));
