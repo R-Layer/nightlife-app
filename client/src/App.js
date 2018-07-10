703b45d56ad19d76350adcaf056a5117597ed657
R-Layer <layer@europe.com>
2018-07-10 18:56:02 +0200

    Boilerplate bulma front-back

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,40 @@
 import React, { Component } from "react";
+
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 import "./css/App.css";
 import "./css/customBulma.css";
 
+import { rootReducer } from "./reducers/rootReducer";
+
+import PrivateRoute from "./components/PrivateRoute";
+import LandingPage from "./components/LandingPage";
+import LandingPrivate from "./components/LandingPrivate";
+
 class App extends Component {
   render() {
+    const store = createStore(rootReducer, applyMiddleware(thunk));
+    const author = { isAuthenticated: true };
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro has-background-primary">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Provider store={store}>
+          <div>
+            <Route exact path={"/"} component={LandingPage} />
+            <Switch>
+              <PrivateRoute
+                exact
+                path={"/private"}
+                auth={author}
+                component={LandingPrivate}
+              />
+            </Switch>
+          </div>
+        </Provider>
+      </Router>
     );
   }
 }
---
+++ client/src/components/LandingPrivate.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+
+class LandingPrivate extends Component {
+  render() {
+    return (
+      <section className="hero is-primary">
+        <div className="hero-body">
+          <div className="container">
+            <h1 className="title">Private Land</h1>
+          </div>
+        </div>
+      </section>
+    );
+  }
+}
+
+export default LandingPrivate;
